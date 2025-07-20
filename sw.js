4d5c4ec7e175be5afe1925c4733bd3e182f7b6f4
CHAHBG <93495827+CHAHBG@users.noreply.github.com>
2025-07-20 23:18:38 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
+const CACHE='boundou-cache-v1';
+const OFFLINE_URL='/offline.html';
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll([
+      '/','index.html','bundle.js','/style.css','style.min.css',
+      'data/communes_boundou.topo.json','data/parcelles.json'
+    ]))
+  );
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
 
